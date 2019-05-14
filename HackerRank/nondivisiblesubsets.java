6c339637a32f04aa2569620ff22f8cee2afc74d8
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-15 00:33:06 +0530

    non divisible sets

--- HackerRank/nondivisiblesubsets.java
+++ HackerRank/nondivisiblesubsets.java
@@ -2,28 +2,36 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class nondivisiblesubsets {
+    static Random rand;
 
     public static ArrayList<Integer> generateSubset(int[] s, int n) {
-        Random rand = new Random();
+        rand = new Random();
         int size = s.length;
         ArrayList<Integer> tempArr = new ArrayList<Integer>();
         for(int i=0; i< n; i++) {
-            tempArr.add(rand.nextInt(size));
+            int temp = rand.nextInt(size);
+            boolean found = false;
+            for(int k =0; k< tempArr.size(); k++) {
+                if (tempArr.get(k) == s[temp]) 
+                    found = true;
+            }
+            if (!found)
+                tempArr.add(s[temp]);
+            else 
+                n += 1;
         }
         return tempArr;
     }
 
     public static Boolean checkSumDivByk(ArrayList<Integer> s, int k) {
-        System.out.println("s is " + s);
         int size = s.size(), sum=0;
         for(int i=0; i<size; i++) {
             for(int j=i+1; j<size; j++) {
                 sum = s.get(i) + s.get(j);
                 if (sum % k == 0){
-                    System.out.println("a yes at " + Integer.toString(sum)+ " of "+ Integer.toString(s.get(i)
-                    ) + "  + "+ Integer.toString(s.get(i)));
                     return true;
                 }
             }
@@ -31,25 +39,24 @@ public class nondivisiblesubsets {
         return false;
     }
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()), k = Integer.parseInt(br.readLine());
+        Scanner br = new Scanner(System.in);
+        int n = br.nextInt(), k = br.nextInt();
         int temp = 0, set[] = new int[n];
         for(temp = 0; temp < n; temp ++) { // taking inputs
-            set[temp] = Integer.parseInt(br.readLine());
+            set[temp] = br.nextInt();
         }
-        System.out.println("outputs below");
         temp = 0;
         int max =0;
         for (int i =2; i<=n; i++) {
-            for(int j=0; j<n; j++) {
+            for(int j=0; j<n*n; j++) {
                 ArrayList<Integer> arList = generateSubset(set, i);
-                if (checkSumDivByk(arList, k)) {
+                if (!checkSumDivByk(arList, k)) {
                     int len = arList.size();
                     max = len > max ? len : max;
                 }
             }
         }
         System.out.println(max);
-
+        br.close();
     }
 }
--- HackerRank/test.java
+++ HackerRank/test.java
@@ -1,8 +1,11 @@
+import java.util.Random;
+
 public class test {
     public static void main(String[] args) {
-        int a = 10;
-        while (a--!=0) {
-            System.out.println(a);
+        Random rand = new Random();
+        int n = 100;
+        while (n-- != 0) {
+            System.out.println(rand.nextInt(10));
         }
     }
 }
