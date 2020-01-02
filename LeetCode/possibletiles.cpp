b4c3ca9514751be28ed4bc6e2765f507d8ae907e
muskankhedia <muskan.khedia2000@gmail.com>
2020-01-03 00:19:34 +0530

    4

--- LeetCode/possibletiles.cpp
+++ LeetCode/possibletiles.cpp
@@ -8,19 +8,28 @@
 using namespace std;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 int main()
 {
 =======
 int main() {
 >>>>>>> 7a43798... 1
+=======
+int main()
+{
+>>>>>>> 04465d8... 2
 
     string input;
     cin >> input;
     cout << input;
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 =======
     
 >>>>>>> 7a43798... 1
     return 0;
+=======
+    a return 0;
+>>>>>>> 04465d8... 2
 }
